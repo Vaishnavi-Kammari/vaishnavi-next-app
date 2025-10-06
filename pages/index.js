0a379454668d5bad793f0d2ec0b613abd4e8c87a
Vaishnavi <vyshukammari@gmail.com>
2025-10-06 19:36:31 +0530

    Update index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello from Next.js!</h1>
+      <p>This application is containerized, automated with GitHub Actions, and deployable on Minikube.</p>
+    </div>
+  );
+}
